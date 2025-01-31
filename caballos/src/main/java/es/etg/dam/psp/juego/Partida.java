ca2db0baff3583f182b9bf382e630eb67e538d1d
Amanda Sofia Migut Zugaj <amandamigut@gmail.com>
2025-01-31 22:53:04 +0100

    funciona Ok

--- caballos/src/main/java/es/etg/dam/psp/juego/Partida.java
+++ caballos/src/main/java/es/etg/dam/psp/juego/Partida.java
@@ -12,50 +12,52 @@ public class Partida {
     public static final int MAX_CABALLOS = 4;
     public static final int MAX_PUNTOS = 50;
     private Boolean partidaEnCurso = true;
-    private List<Caballo> caballos = new ArrayList<>(); 
+    private List<Caballo> caballos = new ArrayList<>();
 
-    public synchronized void empezarPartida() throws InterruptedException, IOException{
-        while (partidaEnCurso){
+    public synchronized void empezarPartida() throws InterruptedException, IOException {
+        while (partidaEnCurso) {
             avanzar();
-    }
+        }
     }
 
-    public void avanzar() throws InterruptedException, IOException{
-        while (partidaEnCurso){
+    public void avanzar() throws InterruptedException, IOException {
+        while (partidaEnCurso) {
             Thread.sleep(200);
             int puntos = numeroAleatorio(MAX_PUNTOS_TURNO);
             Caballo caballoTurno = caballos.get(numeroAleatorio(caballos.size()));
             Utilidades.enviar(puntos, caballoTurno.getConexion());
             caballoTurno.setPuntos(puntos);
-            if (comprobarGanador(caballoTurno)){
+            if (comprobarGanador(caballoTurno)) {
                 partidaEnCurso = false;
                 enviarMensajeFinal(caballoTurno);
             }
         }
-        
     }
-    private void enviarMensajeFinal(Caballo caballo) throws IOException{
-        for (Caballo cliente : caballos){
-            if (cliente == caballo){
+
+    private void enviarMensajeFinal(Caballo caballo) throws IOException {
+        for (Caballo cliente : caballos) {
+            if (cliente == caballo) {
                 Utilidades.enviar(Utilidades.MENSAJE_GANADOR, cliente.getConexion());
-            }else{
+            } else {
                 Utilidades.enviar(Utilidades.MENSAJE_PERDEDOR, cliente.getConexion());
             }
             cliente.getConexion().close();
         }
     }
 
-    private boolean comprobarGanador(Caballo caballo) throws InterruptedException{
-        if (caballo.getPuntos() >= MAX_PUNTOS){
+    private boolean comprobarGanador(Caballo caballo) throws InterruptedException {
+        if (caballo.getPuntos() >= MAX_PUNTOS) {
             return true;
-            
+
         }
-        return  false;
+        return false;
     }
-    public int getCaballosRegistrados(){
+
+    public int getCaballosRegistrados() {
         return caballos.size();
     }
-    public void aniadir(Caballo caballo){
+
+    public void aniadir(Caballo caballo) {
         caballos.add(caballo);
     }
 
